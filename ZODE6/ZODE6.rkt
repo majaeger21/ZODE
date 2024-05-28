3e94a89bba8f3052d0823ea8c2407ed747b447dc
C.J. DuHamel <cduhamel@calpoly.edu>
2024-05-27 23:03:42 -0700

    big update, i think everything works?

--- ZODE6/ZODE6.rkt
+++ ZODE6/ZODE6.rkt
@@ -168,8 +168,9 @@
 (define (add-store [v : Value] [store : Store]) : Integer
   
   (let ([index (cast (NumV-n (cast (vector-ref store 0) NumV)) Integer)])
+    (println (~v index))
     (cond
-      [(>= (vector-length store) index) (error 'add-store "ZODE: Index out of Bounds Error, attempting to add ~e out of bounds" v)]
+      [(<= (vector-length store) index) (error 'add-store "ZODE: Index out of Bounds Error, attempting to add ~e out of bounds" v)]
       [else (begin (vector-set! store index v)
            (println "This ran")
            
@@ -340,6 +341,8 @@
     [(StrV? s) (begin (printf "~v\n" (StrV-s s)) #t)]
     [else (error 'apply-println "ZODE: Expected a string")]))
 
+(check-exn #rx"ZODE: Ex" (lambda () (apply-println 34)))
+
 ;;Reads a numerical value from stdin
 (define (apply-read-num) : Real
   (printf ">")
@@ -426,25 +429,25 @@ Input: ExprC Env, Output: Value
   (match exp
     [(NumC n) (NumV n)]
     [(StrC str) (StrV str)]
-    #;[(MutC orig new) (mutate-store env store orig new)]
-    #;[(IfC c i e) (let ([condition (interp c env)])
+    [(MutC orig new) (mutate-store env store orig new)]
+    [(IfC c i e) (let ([condition (interp c env store)])
                              (cond
                                [(BoolV? condition) (let ([bool (cast condition BoolV)])
                                                      (cond
-                                                       [(BoolV-b bool) (interp i env)]
-                                                       [else (interp e env)]))]
+                                                       [(BoolV-b bool) (interp i env store)]
+                                                       [else (interp e env store)]))]
                                [else (error 'interp "ZODE: Expected a condition,
                                                          got ~e instead" c)]))]
     [(AppC expr args) (let ([clo (let ([temp-clo (interp expr env store)])
                                     (cond
-                                      ;[(CloV? temp-clo) (cast temp-clo CloV)]
+                                      [(CloV? temp-clo) (cast temp-clo CloV)]
                                       [(PrimV? temp-clo) (cast temp-clo PrimV)]
                                       [else (begin (printf "~v" expr)(error 'interp "ZODE: Expected CloV
                                                       or PrimV, got ~e in ~e" temp-clo expr))]))])
                      (match clo
-                       #;[(? CloV?)(cond
+                       [(? CloV?)(cond
                          [(= (length args) (length (CloV-args clo))) (interp
-                                      (CloV-body clo) (add-env (CloV-env clo) (interp-args args env) (CloV-args clo)))]
+                                      (CloV-body clo) (add-env (CloV-env clo) (interp-args args env store) (CloV-args clo) store) store)]
                          [else (error 'interp "ZODE: Number of Argument Mismatch, expected
                                ~e - ~e, got ~e - ~e" (length (CloV-args clo)) (CloV-args clo) (length args) args)])]
                        [(? PrimV?) (apply-func (PrimV-p clo) (interp-args args env store) store)]))]
@@ -453,9 +456,14 @@ Input: ExprC Env, Output: Value
     [(IdC s) (interp-id s env store)]
     ))
 
+
+
 ;;top-interp
 (define (top-interp [s : Sexp] [memsize : Integer]) : String
-  (serialize (interp (parse s) top-env (cast (make-vector memsize) Store))))
+  (serialize (interp (parse s) top-env (create-store memsize))))
+
+
+(check-equal? (top-interp '{locals : i = 0 : {i := 2}} 35) "null")
 
 
 ;; PROGRAM: Euclidean Algorithm for GCD
@@ -581,7 +589,7 @@ Results:
   (check-equal? (vector-ref store 17) (NumV 0.0))
   (check-equal? (vector-ref store 18) (NumV 0.0))
   (check-equal? (vector-ref store 0) (NumV 19)))
-'apply-func "ZODE: make-array expects 2 arguments"
+
 
 (let ([store (create-store 25)])
   ;; Test make-array with invalid size 0
@@ -715,7 +723,7 @@ Results:
 
 
 ;interp test cases
-#;(check-equal? (interp (AppC (IdC '+) (list (AppC (LambC (list 'x 'y) (AppC (IdC '+)
+(check-equal? (interp (AppC (IdC '+) (list (AppC (LambC (list 'x 'y) (AppC (IdC '+)
                             (list (IdC 'x) (IdC 'y)))) (list (NumC 3) (NumC 5))) (NumC 2))) top-env (create-store 35)) (NumV 10) )
 
 #;(check-exn #rx"ZODE: Expected CloV" (lambda () (interp (AppC (IdC '+) (list (AppC
@@ -730,49 +738,49 @@ Results:
 (check-equal? (interp (AppC (IdC '/) (list (NumC 1) (NumC 2))) top-env (create-store 35)) (NumV 1/2))
 (check-equal? (interp (AppC (IdC '+) (list (NumC 1) (NumC 2))) top-env (create-store 35)) (NumV 3))
 
-#;(check-equal? (interp (AppC (IdC 'equal?) (list (NumC 3) (AppC (IdC '+) (list
-                                                     (NumC 1) (NumC 2))))) top-env) (BoolV #t))
-#;(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (NumC 3) (NumC 3))) (AppC (IdC '+)
-                                                (list (NumC 1) (NumC 2))) (NumC 1)) top-env) (NumV 3))
-#;(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (StrC "my string") (StrC "my string")))
-                          (AppC (IdC '+) (list (NumC 1) (NumC 2))) (NumC 1)) top-env) (NumV 3))
-#;(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (IdC 'false) (IdC 'false)))
-                           (AppC (IdC '+) (list (NumC 1) (NumC 2))) (NumC 1)) top-env) (NumV 3))
-#;(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (NumC 3) (NumC 4)))
-                           (AppC (IdC '+) (list (NumC 1) (NumC 2))) (NumC 1)) top-env) (NumV 1))
-#;(check-exn #rx"ZODE: Expected a condition" (lambda () (interp (IfC (NumC 4) (AppC (IdC '+)
-                                                  (list (NumC 1) (NumC 2))) (NumC 1)) top-env)))
-#;(check-exn #rx"ZODE: No parameter" (lambda () (interp (IfC (AppC (IdC 'equal?) (list (NumC 3)
-                                     (NumC 3))) (AppC (IdC 'z) (list (NumC 1) (NumC 2))) (NumC 1)) top-env)))
+(check-equal? (interp (AppC (IdC 'equal?) (list (NumC 3) (AppC (IdC '+) (list
+                                                     (NumC 1) (NumC 2))))) top-env (create-store 35)) (BoolV #t))
+(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (NumC 3) (NumC 3))) (AppC (IdC '+)
+                                                (list (NumC 1) (NumC 2))) (NumC 1)) top-env (create-store 35)) (NumV 3))
+(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (StrC "my string") (StrC "my string")))
+                          (AppC (IdC '+) (list (NumC 1) (NumC 2))) (NumC 1)) top-env (create-store 35)) (NumV 3))
+(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (IdC 'false) (IdC 'false)))
+                           (AppC (IdC '+) (list (NumC 1) (NumC 2))) (NumC 1)) top-env (create-store 35)) (NumV 3))
+(check-equal? (interp (IfC (AppC (IdC 'equal?) (list (NumC 3) (NumC 4)))
+                           (AppC (IdC '+) (list (NumC 1) (NumC 2))) (NumC 1)) top-env (create-store 35)) (NumV 1))
+(check-exn #rx"ZODE: Expected a condition" (lambda () (interp (IfC (NumC 4) (AppC (IdC '+)
+                                                  (list (NumC 1) (NumC 2))) (NumC 1)) top-env (create-store 35))))
+(check-exn #rx"ZODE: No parameter" (lambda () (interp (IfC (AppC (IdC 'equal?) (list (NumC 3)
+                                     (NumC 3))) (AppC (IdC 'z) (list (NumC 1) (NumC 2))) (NumC 1)) top-env (create-store 35))))
 
 ;top interp tests
-#;(check-equal? (top-interp '{locals : x = 12 : {+ x 1}}) "13")
-#;(check-equal? (top-interp '{locals : x = false : x}) "false")
-#;(check-equal? (top-interp '{locals : x = true : x}) "true")
-#;(check-equal? (top-interp '{if : {equal? "mystring" "mystring"} :
-                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}}) "#<procedure>")
-#;(check-equal? (top-interp '{if : {equal? "mystring" "mystring"} : + : {lamb : y : {- y 34}}}) "#<primop>")
-#;(check-equal? (top-interp '{if : {equal? "mystring" "mystring"} : "mystring" : {lamb : y : {- y 34}}}) "\"mystring\"")
-#;(check-exn #rx"user-error: \"this" (lambda () (top-interp '{if : {equal? "mystring" "mystring"} :
-                                                             {error "this didnt work"} : {lamb : y : {- y 34}}})))
-#;(check-exn #rx"user-error" (lambda () (top-interp '((lamb : e : (e e)) error))))
-#;(check-equal? (top-interp '{locals : add1 = {lamb : x : {+ x 1}} : y = {+ 3 4} : {add1 y}}) "8")
+(check-equal? (top-interp '{locals : x = 12 : {+ x 1}} 35) "13")
+(check-equal? (top-interp '{locals : x = false : x} 35) "false")
+(check-equal? (top-interp '{locals : x = true : x} 35) "true")
+(check-equal? (top-interp '{if : {equal? "mystring" "mystring"} :
+                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}} 35) "#<procedure>")
+(check-equal? (top-interp '{if : {equal? "mystring" "mystring"} : + : {lamb : y : {- y 34}}} 35) "#<primop>")
+(check-equal? (top-interp '{if : {equal? "mystring" "mystring"} : "mystring" : {lamb : y : {- y 34}}} 35) "\"mystring\"")
+(check-exn #rx"user-error: \"this" (lambda () (top-interp '{if : {equal? "mystring" "mystring"} :
+                                                             {error "this didnt work"} : {lamb : y : {- y 34}}} 35)))
+(check-exn #rx"user-error" (lambda () (top-interp '((lamb : e : (e e)) error) 35)))
+(check-equal? (top-interp '{locals : add1 = {lamb : x : {+ x 1}} : y = {+ 3 4} : {add1 y}} 35) "8")
 
-#;(check-equal? (top-interp '{if : {equal? {lamb : x : {+ x 34}} {lamb : x : {+ x 34}}} :
-                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}}) "#<procedure>")
-#;(check-equal? (top-interp '{if : {equal? + +} :
-                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}}) "#<procedure>")
-#;(check-equal? (top-interp '{if : {equal? + "string"} :
-                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}}) "#<procedure>")
+(check-equal? (top-interp '{if : {equal? {lamb : x : {+ x 34}} {lamb : x : {+ x 34}}} :
+                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}} 35) "#<procedure>")
+(check-equal? (top-interp '{if : {equal? + +} :
+                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}} 35) "#<procedure>")
+(check-equal? (top-interp '{if : {equal? + "string"} :
+                               {lamb : x : {+ x 34}} : {lamb : y : {- y 34}}} 35) "#<procedure>")
 
-#;(check-equal? (top-interp '{seq {+ 3 4} {println "print this"} {- 4 3}}) "1")
-#;(check-equal? (top-interp '{seq {+ 3 4} {println {++ "print this" " and then this"}} {- 4 3}}) "1")
+(check-equal? (top-interp '{seq {+ 3 4} {println "print this"} {- 4 3}} 35) "1")
+(check-equal? (top-interp '{seq {+ 3 4} {println {++ "print this" " and then this"}} {- 4 3}} 35) "1")
 
-;(check-equal? (top-interp '{locals : x = {read-num} : {+ x 4}}) "8")
-;(check-equal? (top-interp '{locals : x = {read-str} : x}) "\"hello world\"")
-#;(check-exn #rx"ZODE: No expressions" (lambda () (top-interp '{seq})))
+;(check-equal? (top-interp '{locals : x = {read-num} : {+ x 4}} 35) "8")
+;(check-equal? (top-interp '{locals : x = {read-str} : x} 35) "\"hello world\"")
+(check-exn #rx"ZODE: No expressions" (lambda () (top-interp '{seq} 35)))
 
-#;(check-equal?
+(check-equal?
  (top-interp '{locals : Y = {lamb : f : {{lamb : x : {x x}}
                                          {lamb : x : {f {lamb : a : {{x x} a}}}}}}
                       : empty = "mt"
@@ -783,6 +791,6 @@ Results:
                       {locals : sum = {Y {lamb : sum : {lamb : lst : {if : {empty? lst}
                                                                          : 0
                                                                          : {+ {first lst} {sum {rest lst}}}}}}}
-                              : {sum {cons 10 {cons 20 {cons 30 {cons 40 empty}}}}}}})
+                              : {sum {cons 10 {cons 20 {cons 30 {cons 40 empty}}}}}}} 500)
 
  "100")
